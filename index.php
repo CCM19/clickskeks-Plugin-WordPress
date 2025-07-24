d981643456b8fde5eed76318a058225817b8ac9a
Jona-St <js@papoo.de>
2025-07-24 12:08:05 +0200

    bump version to 1.4.9

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Clickskeks
  * description: Integrates the Clickskeks DSGVO solution into WordPress
- * Version: 1.4.8
+ * Version: 1.4.9
  * Author: Papoo Software &amp; Media GmbH
  * Author URI: https://papoo-media.de
  * License: GPLv2 or later
